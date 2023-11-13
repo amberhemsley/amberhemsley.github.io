4e2c917f0a9fa61d7fa0c7a10c4f3066e2a761a0
amberhemsley <amber.hemsley@dojo.tech>
2023-11-13 09:44:46 +0000

    contact

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -12,19 +12,91 @@ const Experience = () => {
                 <meta charSet="UTF-8"/>
                 <title>Experience</title>
             </head>
-            <body>
+            <body className="App">
             <Tabs>
                 <TabList>
-                    <Tab>Work</Tab>
+                    <Tab >Work</Tab>
                     <Tab>Uni</Tab>
                     <Tab>School</Tab>
                 </TabList>
 
                 <TabPanel>
                     <h2>Any content 1</h2>
+                    <Tabs>
+                        <TabList>
+                            <Tab>DOJO</Tab>
+                            <Tab>other</Tab>
+                        </TabList>
+
+                        <TabPanel>
+                            <h2>Go, kotlin, kubernetes, redis, rest, websockets, mongo, hexagonal architecture, pubsub</h2>
+                        </TabPanel>
+                        <TabPanel>
+                            <h2>Any content 1</h2>
+                        </TabPanel>
+                    </Tabs>
                 </TabPanel>
                 <TabPanel>
                     <h2>Any content 2</h2>
+                    <Tabs>
+                        <TabList>
+                            <Tab>Projects</Tab>
+                            <Tab>Modules</Tab>
+                        </TabList>
+
+                        <TabPanel>
+                            <h2>Any content 1</h2>
+                        </TabPanel>
+                        <TabPanel>
+                            <h2>Any content 1</h2>
+                        </TabPanel>
+                    </Tabs>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 3</h2>
+                    <Tabs>
+                        <TabList>
+                            <Tab>Projects</Tab>
+                            <Tab>Subjects</Tab>
+                        </TabList>
+
+                        <TabPanel>
+                            <h2>Any content 1</h2>
+                        </TabPanel>
+                        <TabPanel>
+                            <h2>Any content 1</h2>
+                        </TabPanel>
+                    </Tabs>
+                    <div className="table-right">
+                    <table>
+                        <thead>
+                        <tr>
+                            <th colSpan="2">Results</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th>Subject</th>
+                            <th>Grade</th>
+                        </tr>
+                        <tr>
+                            <td>Computer Science</td>
+                            <td>A*</td>
+                        </tr>
+                        <tr>
+                            <td>Maths</td>
+                            <td>A*</td>
+                        </tr>
+                        <tr>
+                            <td>Physics</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>Further Maths AS</td>
+                            <td>A</td>
+                        </tr>
+                        </tbody>
+                    </table></div>
                 </TabPanel>
             </Tabs>
             </body>
