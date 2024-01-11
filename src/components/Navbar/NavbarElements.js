450328c7746ff1c4a665dcc59d5889eeda472171
amberhemsley <amber.hemsley@dojo.tech>
2024-01-11 14:13:04 +0000

    colours

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -3,7 +3,7 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-background: #ffb3ff;
+background: #FFFFFF;
 height: 85px;
 display: flex;
 justify-content: space-between;
@@ -12,7 +12,7 @@ z-index: 12;
 `;
 
 export const NavLink = styled(Link)`
-color: #808080;
+color: #16221D;
 display: flex;
 align-items: center;
 text-decoration: none;
@@ -20,13 +20,13 @@ padding: 0 1rem;
 height: 100%;
 cursor: pointer;
 &.active {
-	color: #4d4dff;
+	color: #DA8FB1;
 }
 `;
 
 export const Bars = styled(FaBars)`
 display: none;
-color: #808080;
+color: #DA8FB1;
 @media screen and (max-width: 768px) {
 	display: block;
 	position: absolute;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,11 @@ import logo from '../logo.svg';
 import '../App.css';
 import App from "../App";
 
-
+// create a React component that renders a buisness card which flips when its clicked on
+function flipCard() {
+    var card = document.getElementById("card");
+    card.classList.toggle("flip");
+}
 const Contact = () => {
     return (
         <>
@@ -15,6 +19,16 @@ const Contact = () => {
                 <div>
                     <p>Email: amberhemsley02@gmail.com</p>
                     <p>LinkedIn: <a href="https://uk.linkedin.com/in/amber-hemsley-7061971b8">Amber Hemsley</a></p>
+                </div>
+                <div className="card" onClick={flipCard}>
+                    <div className="front">
+                        <p>Amber Hemsley</p>
+                        <p>Software Engineer</p>
+                    </div>
+                    <div className="back">
+                        <p>Amber Hemsley</p>
+                        <p>Software Engineer</p>
+                    </div>
                 </div>
             </body>
         </>
