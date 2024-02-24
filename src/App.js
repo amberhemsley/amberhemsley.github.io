a1930ee13db2fbb4a78641a90ce6f05446b819bd
amberhemsley <amber.hemsley@dojo.tech>
2024-02-24 22:25:06 +0000

    browser router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import './App.css';
 import TopBar from './components/TopBar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route }
+  from 'react-router-dom';
 import Home from './pages/home';
 import Experience from './pages/experience';
 import Misc from './pages/misc';
 import Contact from './pages/contact';
 
 function App() {
-    return (
-        <Router basename="/amberhemsley.github.io">
-            <TopBar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/experience" element={<Experience />} />
-                <Route path="/misc" element={<Misc />} />
-                <Route path="/contact" element={<Contact />} />
-            </Routes>
-        </Router>
-    );
+  return (
+      <Router >
+        <TopBar />
+        <Routes>
+          <Route exact path='/' exact element={<Home/>} />
+          <Route path='/experience' element={<Experience/>} />
+          <Route path='/misc' element={<Misc/>} />
+          <Route path='/contact' element={<Contact/>} />
+        </Routes>
+      </Router>
+  );
 }
 
 export default App;
