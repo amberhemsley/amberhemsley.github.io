f469b1a2eb0674ed5b4543d9188eeff3351fcdb6
amberhemsley <amber.hemsley@dojo.tech>
2024-02-24 22:38:04 +0000

    fix deployment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import './App.css';
 import TopBar from './components/TopBar';
-import { BrowserRouter as Router, Routes, Route }
-  from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Experience from './pages/experience';
 import Misc from './pages/misc';
 import Contact from './pages/contact';
 
 function App() {
-  return (
-      <Router >
-        <TopBar />
-        <Routes>
-          <Route exact path='/' exact element={<Home/>} />
-          <Route path='/experience' element={<Experience/>} />
-          <Route path='/misc' element={<Misc/>} />
-          <Route path='/contact' element={<Contact/>} />
-        </Routes>
-      </Router>
-  );
+    return (
+        <Router >
+            <TopBar />
+            <Routes>
+                <Route exact path='/' exact element={<Home/>} />
+                <Route path='/experience' element={<Experience/>} />
+                <Route path='/misc' element={<Misc/>} />
+                <Route path='/contact' element={<Contact/>} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
