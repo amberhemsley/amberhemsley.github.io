25bcb6a1f8c4d15562cc4c63ebefbff043f79f2f
amberhemsley <amber.hemsley@dojo.tech>
2024-02-27 21:54:11 +0000

    contact card

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,12 +3,12 @@ import '../App.css';
 import App from "../App";
 import PermanentDrawer from "../components/Menu/PermanentDrawer";
 import React from "react";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import {Card, CardActions, CardContent, Chip, Grid} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
-// create a React component that renders a buisness card which flips when its clicked on
-function flipCard() {
-    var card = document.getElementById("card");
-    card.classList.toggle("flip");
-}
 const Contact = () => {
     return (
         <div className="contact-page">
@@ -18,30 +18,27 @@ const Contact = () => {
 
             </head>
             <body>
-                <div style={{display:"flex",justifyContent: "center", padding:`10px`}}>
-                    <div style={
-                        {
-                            display:"flex",
-                            background:'#EFEFEF',
-                            alignItems:"center",
-                            flexDirection:"row",
-                            borderRadius:'25px',
-                            padding: '40px',
-                            width:"fit-content",
-                        }
-                    }>
-                        <div style={{display:"flex", flexDirection:"column", alignItems:"start", paddingRight:20}}>
-                            <text style={{color:"#35605A", fontWeight: "bold", fontSize: 18}}>Email: </text>
-                            <line></line>
-                            <text style={{color:"#35605A", fontWeight: "bold", fontSize: 18}}>LinkedIn: </text>
-
-                        </div>
-                        <div style={{display:"flex", flexDirection:"column", alignItems:"start", paddingLeft:'10px'}}>
-                            <a href="mailto:amberhemsley02@gmail.com" style={{color:'#DA8FB1'}}> amberhemsley02@gmail.com</a>
-                            <line></line>
-                            <a href="https://uk.linkedin.com/in/amber-hemsley-7061971b8" style={{color:'#DA8FB1'}}> Amber Hemsley</a>
-                        </div>
-                    </div>
+                <div style={{display:"flex",justifyContent: "center", padding:`20px`}}>
+                    <Card sx={{ width: '50vw',maxWidth:'400px', backgroundColor:'rgba(0, 0, 0, 0.05)'}}>
+                        <CardContent>
+                            <Grid container spacing={0}>
+                                <Grid item xs={4}>
+                                    <Chip label="Email:" icon={<AlternateEmailIcon  sx={{color:'#0077b5'}}/>} sx={{backgroundColor:'rgba(0, 0, 0, 0)', fontSize:'medium'}}/>
+                                </Grid>
+                                <Grid item xs={8}>
+                                    <Chip label={<a href="mailto:amberhemsley02@gmail.com" style={{color:'#DA8FB1'}}> amberhemsley02@gmail.com</a>}
+                                          sx={{backgroundColor:'rgba(0, 0, 0, 0)', fontSize:'medium'}}/>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <Chip label="LinkedIn:" icon={<LinkedInIcon  sx={{color:'#0077b5'}}/>} sx={{backgroundColor:'rgba(0, 0, 0, 0)', fontSize:'medium'}}/>
+                                </Grid>
+                                <Grid item xs={8}>
+                                    <Chip label={<a href="https://uk.linkedin.com/in/amber-hemsley-7061971b8" style={{color:'#DA8FB1'}}> Amber Hemsley</a>}
+                                          sx={{backgroundColor:'rgba(0, 0, 0, 0)', fontSize:'medium'}}/>
+                                </Grid>
+                            </Grid>
+                        </CardContent>
+                    </Card>
                 </div>
             </body>
         </div>
