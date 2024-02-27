664447b1df43ca9162edbb10f95be18beafff22c
amberhemsley <amber.hemsley@dojo.tech>
2024-02-27 22:02:38 +0000

    contact card

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,6 +8,7 @@ import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import {Card, CardActions, CardContent, Chip, Grid} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Block from "../components/Menu/block";
 
 const Contact = () => {
     return (
@@ -18,27 +19,25 @@ const Contact = () => {
 
             </head>
             <body>
-                <div style={{display:"flex",justifyContent: "center", padding:`20px`}}>
-                    <Card sx={{ width: '50vw',maxWidth:'400px', backgroundColor:'rgba(0, 0, 0, 0.05)'}}>
-                        <CardContent>
-                            <Grid container spacing={0}>
-                                <Grid item xs={4}>
-                                    <Chip label="Email:" icon={<AlternateEmailIcon  sx={{color:'#0077b5'}}/>} sx={{backgroundColor:'rgba(0, 0, 0, 0)', fontSize:'medium'}}/>
-                                </Grid>
-                                <Grid item xs={8}>
-                                    <Chip label={<a href="mailto:amberhemsley02@gmail.com" style={{color:'#DA8FB1'}}> amberhemsley02@gmail.com</a>}
-                                          sx={{backgroundColor:'rgba(0, 0, 0, 0)', fontSize:'medium'}}/>
-                                </Grid>
-                                <Grid item xs={4}>
-                                    <Chip label="LinkedIn:" icon={<LinkedInIcon  sx={{color:'#0077b5'}}/>} sx={{backgroundColor:'rgba(0, 0, 0, 0)', fontSize:'medium'}}/>
-                                </Grid>
-                                <Grid item xs={8}>
-                                    <Chip label={<a href="https://uk.linkedin.com/in/amber-hemsley-7061971b8" style={{color:'#DA8FB1'}}> Amber Hemsley</a>}
-                                          sx={{backgroundColor:'rgba(0, 0, 0, 0)', fontSize:'medium'}}/>
-                                </Grid>
-                            </Grid>
-                        </CardContent>
-                    </Card>
+
+                <div style={{display:"flex",justifyContent: "flex-start", padding:`20px`}}>
+                    <Block/>
+                    <Grid container spacing={0} maxWidth='500px' padding='20px'>
+                        <Grid item xs={4}>
+                            <Chip label="Email:" icon={<AlternateEmailIcon  sx={{color:'#0077b5'}}/>} sx={{backgroundColor:'rgba(0, 0, 0, 0)', fontSize:'calc(0.5vw + 12px)'}}/>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Chip label={<a href="mailto:amberhemsley02@gmail.com" style={{color:'#DA8FB1'}}> amberhemsley02@gmail.com</a>}
+                                  sx={{backgroundColor:'rgba(0, 0, 0, 0)', fontSize:'calc(0.5vw + 12px)'}}/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Chip label="LinkedIn:" icon={<LinkedInIcon  sx={{color:'#0077b5'}}/>} sx={{backgroundColor:'rgba(0, 0, 0, 0)', fontSize:'calc(0.5vw + 12px)'}}/>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Chip label={<a href="https://uk.linkedin.com/in/amber-hemsley-7061971b8" style={{color:'#DA8FB1'}}> Amber Hemsley</a>}
+                                  sx={{backgroundColor:'rgba(0, 0, 0, 0)', fontSize:'calc(0.5vw + 12px)'}}/>
+                        </Grid>
+                    </Grid>
                 </div>
             </body>
         </div>
